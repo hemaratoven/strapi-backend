dc11bb151c90186f90d7036a130288706c1fcdfd
Hemarat Matra (Heam) <hemarat@shareinvestor.com>
2024-01-31 13:03:22 +0700

    connect storage to Media Library

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,4 +1,4 @@
-module.exports = [
+module.exports = ({ env }) => [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
@@ -9,4 +9,28 @@ module.exports = [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            env("CF_PUBLIC_ACCESS_URL").replace(/^https?:\/\//, ""),
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            env("CF_PUBLIC_ACCESS_URL").replace(/^https?:\/\//, ""),
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
 ];
